59e41423225d8b85125e15bcf1b3d1afa3ebdff0
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-31 21:32:06 +0000

    Begin working on ClientsController

---
+++ src/main/java/com/example/tutorial_baeldung_react_spring_boot/controller/ClientsController.java
@@ -0,0 +1,11 @@
+package com.example.tutorial_baeldung_react_spring_boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientsController {
+
+
+}

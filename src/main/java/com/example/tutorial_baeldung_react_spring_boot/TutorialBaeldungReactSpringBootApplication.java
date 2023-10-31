c0ba5c513e812e717ce8aa95166e0bbffeffee64
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-31 21:20:48 +0000

    Initial commit

---
+++ src/main/java/com/example/tutorial_baeldung_react_spring_boot/TutorialBaeldungReactSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorial_baeldung_react_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialBaeldungReactSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialBaeldungReactSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tutorial_baeldung_react_spring_boot/TutorialBaeldungReactSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorial_baeldung_react_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialBaeldungReactSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

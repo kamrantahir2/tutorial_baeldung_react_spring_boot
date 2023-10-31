ae3715ee2a155952ee27a5b40d50e0c6b20e9102
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-31 21:39:26 +0000

    Add findAll and findById Get mapping on /clients url

--- src/main/java/com/example/tutorial_baeldung_react_spring_boot/controller/ClientsController.java
+++ src/main/java/com/example/tutorial_baeldung_react_spring_boot/controller/ClientsController.java
@@ -1,11 +1,36 @@
 package com.example.tutorial_baeldung_react_spring_boot.controller;
 
+import com.example.tutorial_baeldung_react_spring_boot.model.Client;
+import com.example.tutorial_baeldung_react_spring_boot.repository.ClientRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientsController {
+
+    private final ClientRepository clientRepository;
+
+    public ClientsController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @GetMapping
+    public List<Client> getClients() {
+        return clientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Client getClient(@PathVariable Long id) {
+        return clientRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+
+
 
 
 }
